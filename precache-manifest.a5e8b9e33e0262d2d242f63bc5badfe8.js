77171f30693606eae54c2808b336c29249b455a9
X-olani <xolani.sk.yona@gmail.com>
2021-08-10 22:26:33 +0200

    first commit

---
+++ precache-manifest.a5e8b9e33e0262d2d242f63bc5badfe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/logo.d41d8cd9.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b21bcdde3d472fb8201",
+    "url": "/static/js/main.1b21bcdd.chunk.js"
+  },
+  {
+    "revision": "c412483ab97a1b4f0311",
+    "url": "/static/js/1.c412483a.chunk.js"
+  },
+  {
+    "revision": "1b21bcdde3d472fb8201",
+    "url": "/static/css/main.e2676c06.chunk.css"
+  },
+  {
+    "revision": "c25f16d7f4d5da5be325e65c03612890",
+    "url": "/index.html"
+  }
+];
